1190614861438035a24815fcedcf2252eb887d72
Osama <shaikhosama499@gmail.com>
2024-06-04 18:48:27 +0530

    chore(app): console log removed and display message added

--- src/components/BookContainer.jsx
+++ src/components/BookContainer.jsx
@@ -14,7 +14,6 @@ const BookContainer = () => {
     e.preventDefault();
     const searchQuery = e.target.elements.query.value;
     dispatch(setQuery(searchQuery));
-    console.log(searchQuery);
   };
 
   return (
--- src/pages/MyBookShelf.jsx
+++ src/pages/MyBookShelf.jsx
@@ -4,14 +4,18 @@ import { useSelector } from "react-redux"
 
 const MyBookShelf = () => {
     const books = useSelector((store)=>store?.myShelf?.books);
-    console.log(books);
   return (
     <div className="sm:mx-1 xl:mx-36 lg:mx-20 flex flex-col items-center gap-5 ">
         <div className='text-2xl'>My BookShelf</div>
         <div className="grid xl:grid-cols-5 lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-3 gap-x-5 gap-y-10">
-        {books.map((book) => (
+        {books?.length> 0 ?(books.map((book) => (
             <BookCard key={book.key} book={book} showAddButton={false}/>
-          ))}
+          ))):(
+            <div className="col-span-5 flex flex-col items-center justify-center pt-20">
+            <h1 className="text-xl text-[#edae2f]">
+            Add Your Favourite Books in My Book Shelf to read it later!!</h1>
+            </div>
+            )}
         </div>
     </div>
   )
